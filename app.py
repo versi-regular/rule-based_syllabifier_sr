e3a3e69876722328ba83637ce620f35fa6102c62
versi-regular <poet4lifetime@gmail.com>
2021-11-10 19:43:32 +0100

    First form attempt

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Helo World'
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html', form_data=form_data)
+
 
 if __name__ == '__main__':
     app.run()
