d53e4538df7f40c4f5eef43148c4443561da7973
versi-regular <poet4lifetime@gmail.com>
2021-11-10 19:58:24 +0100

    Form attempt 2

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def form():
-    return render_template("syllabifier.html")
+    return render_template('syllabifier.html')
+
 
 if __name__ == '__main__':
     app.run()
