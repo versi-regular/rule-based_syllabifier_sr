1c46e847945041f78a41ad58a64a262ccf2b80f3
versi-regular <poet4lifetime@gmail.com>
2021-11-10 19:48:23 +0100

    Simplification attempt

--- app.py
+++ app.py
@@ -8,14 +8,5 @@ def form():
     return render_template('form.html')
 
 
-@app.route('/data', methods=['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html', form_data=form_data)
-
-
 if __name__ == '__main__':
     app.run()
