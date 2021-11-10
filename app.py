fc4630d6724331910b3bda17b8ed4c4a4465e42c
versi-regular <poet4lifetime@gmail.com>
2021-11-10 21:30:52 +0100

    First templated app version

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def form():
-    return 'Hello world'
+    return render_template('syllabifier.html')
 
+
+@app.route('/', methods=['POST'])
+def response():
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('syllabified.html', form_data=form_data)
 
 if __name__ == '__main__':
     app.run()
+    #app.run(host='localhost', port=5000)
